9014f68b042fdf76e037d81a4dc8f094ef249bdc
Henry Silva <henry131307@gmail.com>
2023-01-21 15:41:25 -0300

    Create class DaySummaryAPI

--- ingestion.py
+++ ingestion.py
@@ -1,28 +1,42 @@
-import requests
-import logging
+import requests,logging , datetime
+from abc import ABC, abstractmethod
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level="INFO")
 
 
-class MercadoBitCoin():
+class MercadoBitCoin(ABC):
 
     def __init__(self, coin: str) -> None:
         self.coin = coin 
         self.endpoint = "https://www.mercadobitcoin.net/api"
 
-    def _get_endpoint(self) -> str:
-        return f"{self.endpoint}/{self.coin}/day-summary/2023/1/20/"
+    @abstractmethod
+    def _get_endpoint(self,**kwargs) -> str:
+        pass 
+      
 
-    def get_data(self) -> dict:
-        endpoint = self._get_endpoint()
+    def get_data(self,**kwargs) -> dict:
+        endpoint = self._get_endpoint(**kwargs)
         logger.info("Getting data from endpoinr: {endpoint}")
         response = requests.get(endpoint)
         response.raise_for_status()
         return response.json()
 
 
-print(MercadoBitCoin("BTC").get_data())
+class DaySummaryApi(MercadoBitCoin):
+
+    type = "day-summary"
+
+    def _get_endpoint(self, date: datetime.date) -> str:
+        return f"{self.endpoint}/{self.coin}/{self.type}/{date.year}/{date.month}/{date.day}"
+    
+
+print( DaySummaryApi(coin="BTC").get_data(date=datetime.date(2023, 1, 20)))
+
+
+
 
 
 
